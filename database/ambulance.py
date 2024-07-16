46f52e6ffe2ca84bd66a2bc233dc360566a61594
Yuchan Han <serltretu24@gmail.com>
2024-07-17 00:47:33 +0900

    fix: ambulance 모델 비밀번호 저장 삭제

--- database/ambulance.py
+++ database/ambulance.py
@@ -4,6 +4,5 @@ from tortoise import fields
 
 class Ambulance(Model):
     login_id = fields.IntField(pk=True)
-    hashed_password = fields.CharField(null=False, max_length=100)
-    license_plate = fields.CharField(null=False, max_length=10)
+    license_number = fields.CharField(null=False, max_length=10)
     driver = fields.CharField(null=False, max_length=20)
